c09b1908426708bd6825f8b37ffc0eb588cb1d38
Daniel Baur <daniel.baur@uni-ulm.de>
2019-12-19 14:21:19 +0100

    init

--- ci/publish_docker.sh
+++ ci/publish_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-export REPO=dbaur/jCMPL
+export REPO=dbaur/jcmpl
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 docker build -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
